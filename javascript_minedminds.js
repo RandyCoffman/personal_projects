f73b6381395ac8378626ea06930a2afba4622ab8
RandyCoffman <randy.coffman@wvjc.edu>
2018-04-10 11:20:56 -0400

    I added in the triple equals

--- javascript_minedminds.js
+++ javascript_minedminds.js
@@ -1,11 +1,11 @@
 for (var i=1; i <= 100; i++) { 
-	if (i % 5 == 0 && i % 3 == 0) {
+	if (i % 5 === 0 && i % 3 === 0) {
 		console.log("Minedminds"); 
 	}
-	else if (i % 3 == 0) {
+	else if (i % 3 === 0) {
 		console.log("mined"); 
 	}
-	else if (i % 5 == 0) {
+	else if (i % 5 === 0) {
 		console.log("minds"); 
 	}
 	else {
