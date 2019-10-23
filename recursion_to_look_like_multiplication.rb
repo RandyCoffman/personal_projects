9ffb87734c6db4fce3260154bfdeb55e8fdc97f8
Randy Coffman <sc.coffman45@ymail.com>
2019-10-23 10:10:08 -0400

    changed the last elsif to a else since if its's not 0 and it's not grater than 0, then negative numbers will always go here as expected

--- recursion_to_look_like_multiplication.rb
+++ recursion_to_look_like_multiplication.rb
@@ -1,9 +1,9 @@
 def multiplication(x,y)
-	if y == 0 # if y equals the value of 0 then it will return 0 b/c if multiplying by 0 it will always equal 0
-		return 0 # this will return 0
+	if y == 0 
+		return 0
 	elsif y > 0
 		return x + multiplication(x,y-1) # x + (however many times y = y-1 )
-	elsif y < 0
+	else
 		return -multiplication(x,-y)
 	end
 end
